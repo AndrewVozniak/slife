debd0a8cedf4af1ee3a0cf37fff9773aacca2721
Zoriana <z.v.tomkiv@gmail.com>
2024-11-20 15:57:50 +0200

    about photo block

--- resources/views/components/about/photo_section.blade.php
+++ resources/views/components/about/photo_section.blade.php
@@ -1,7 +1,48 @@
 
 <div class="photo_section_wrapper">
     <div class="photo_image_wrapper">
+        <div class="gradient_global"></div>
         <div class="gradient_left"></div>
         <img src="{{ asset('storage/images/backgrounds/bg.jpg') }}" alt="photo" class="photo_image">
+        <div class="gradient_right"></div>
+    </div>
+
+    <div class="text_content">
+        <div class="about_box_bg_wrapper">
+            <div class="about_box_bg_photo">
+                <div class="about_title_bg_photo">
+                    <span >9&#43;
+                     <span style="font-size: 32px"> </span>
+                    </span>
+                </div>
+                <div class="about_text_bg_photo">
+                    <span>{{__('about.years')}}</span>
+                </div>
+            </div>
+
+            <div class="about_box_bg_photo">
+                <div class="about_title_bg_photo">
+                    <span>&#60; 20
+                        <span style="font-size: 32px">{{__('about.thousands')}}</span>
+                    </span>
+                </div>
+
+                <div class="about_text_bg_photo">
+                    <span>{{__('about.clients')}}</span>
+                </div>
+            </div>
+
+            <div class="about_box_bg_photo">
+                <div class="about_title_bg_photo">
+                    <span>&#60; 2
+                        <span style="font-size: 32px">{{__('about.thousands')}}</span>
+                    </span>
+                </div>
+
+                <div class="about_text_bg_photo">
+                    <span>{{__('about.current_offers')}}</span>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
