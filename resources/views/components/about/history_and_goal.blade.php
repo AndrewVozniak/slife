e77d1be8e025f6a33877b70b4c19ace186e37918
Zoriana <z.v.tomkiv@gmail.com>
2024-11-21 21:09:20 +0200

    about section

--- resources/views/components/about/history_and_goal.blade.php
+++ resources/views/components/about/history_and_goal.blade.php
@@ -3,6 +3,7 @@
     <div class="left_upper">
         <img src="{{ asset('storage/images/backgrounds/rectangle.png') }}" alt="photo" class="bg_img">
         <p class="title_goal_and_history left__goal_and_history">{{__('about.our_history_and_goal.goal')}}</p>
+        <img src="{{ asset('storage/images/icons/triangle_left.svg') }}" alt=" " class="left_triangle">
     </div>
     <div class="right_upper">
         <div class="right__goal_and_history">
@@ -14,15 +15,16 @@
     </div>
     <div class="left_bottom">
         <div class="left__goal_and_history">
-            <div class="text__goal_and_history_box">
-                <p class="text_600_24_dark-text">{{__('about.our_history_and_goal.catalog')}}&#46;</p>
-                <p style="color: var(--dark-text2)">{{__('about.our_history_and_goal.we_want')}}&#33;</p>
+            <div class="text__goal_and_history_box left__text_goal_and_history">
+                <p class="text_600_24_dark-text">{{__('about.our_history_and_goal.we_started')}}&#46;</p>
+                <p style="color: var(--dark-text2)">{{__('about.our_history_and_goal.today')}}&#46;</p>
             </div>
         </div>
     </div>
     <div class="right_bottom">
         <img src="{{ asset('storage/images/backgrounds/our_history.png') }}" alt="photo" class="bg_img">
         <p class="title_goal_and_history right__goal_and_history">{{__('about.our_history_and_goal.history')}}</p>
+        <img src="{{ asset('storage/images/icons/triangle_right.svg') }}" alt=" " class="right_triangle">
     </div>
 </div>
 
