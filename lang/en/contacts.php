5ee981816b79fb3f6ec8cfed95a10546c721d20e
AndrewVozniak <a.vozniaks@gmail.com>
2024-12-08 23:36:05 +0100

    Contacts page translation

---
+++ lang/en/contacts.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => 'Contacts',
+    'kopychynci' => 'Kopychyntsi',
+    'ternopilksa_oblast' => 'Ternopil region',
+    'str_chortivska_64' => 'Chortkivska str., 64',
+
+    'from' => 'from',
+    'to' => 'to',
+    'sunday_day_off' => 'Sunday: day off',
+
+    'mark_description_text' => 'Ternopil region, Kopychyntsi, Chortkivska str., 64, 48-260.',
+    'view_on_map' => 'View on map',
+];
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -10,11 +10,11 @@
         <div class="breadcrumbs">
             <a href="{{ route('home', ['locale' => App::currentLocale()])}}" class="breadcrumb">Головна</a>
             <img src="{{asset('storage/images/icons/breadcrumbs_arrow_right.svg')}}" alt="filter" class="param_image">
-            <a href="{{ route('contacts', ['locale' => App::currentLocale()]) }}" class="breadcrumb active">Контакти</a>
+            <a href="{{ route('contacts', ['locale' => App::currentLocale()]) }}" class="breadcrumb active">{{ __('contacts.title') }}</a>
         </div>
 
         <div class="content">
-            <h1 class="content_title">Контакти</h1>
+            <h1 class="content_title">{{ __('contacts.title') }}</h1>
 
             <div class="image_wrapper">
                 <img src="{{ asset('/storage/images/map.png') }}" alt="" class="image">
@@ -23,11 +23,11 @@
                     <div class="contacts__heading">
                         <div class="row">
                             <img src="{{ asset('/storage/images/icons/mage_location.svg') }}" alt="">
-                            <h1 class="title">Копичинці</h1>
+                            <h1 class="title">{{ __('contacts.kopychynci') }}</h1>
                         </div>
 
-                        <span class="contacts_description">Тернопільська обл. <br>
-                            вул. Чортківська, 64 <br>
+                        <span class="contacts_description">{{ __('contacts.ternopilksa_oblast') }} <br>
+                            {{ __('contacts.str_chortivska_64') }} <br>
                             48-260
                         </span>
                     </div>
@@ -53,7 +53,7 @@
 
                     <div class="work_hours">
                         <img src="{{ asset('/storage/images/icons/clock.svg') }}" alt="clock">
-                        <span>з 9:00 до 18:00 <br>Неділя: вихідний</span>
+                        <span>{{ __('contacts.from') }} 9:00 {{ __('contacts.to') }}  18:00 <br>{{ __('contacts.sunday_day_off') }} </span>
                     </div>
 
                     <div class="social__media">
@@ -68,10 +68,10 @@
 
                 <div class="mark__wrapper">
                     <div class="mark__description">
-                        <span class="mark__description__text">Тернопільська обл, м. Копичинці, вул. Чортківська, 64, 48-260.</span>
+                        <span class="mark__description__text">{{ __('contacts.mark_description_text') }}</span>
                         <hr class="horizontal-line">
                         <a href="#" class="mark__description__link">
-                            <span>Подивитись на карті</span>
+                            <span>{{ __('contacts.view_on_map') }}</span>
                             <img src="{{ asset('/storage/images/icons/arrow_right_map.svg') }}" alt="arrow_right">
                         </a>
                     </div>
