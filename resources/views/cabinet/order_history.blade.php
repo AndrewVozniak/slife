b3dd61340860231774fc57432d47ee019116db77
Zoriana <z.v.tomkiv@gmail.com>
2024-12-22 03:18:38 +0200

    order adaptive

--- resources/views/cabinet/order_history.blade.php
+++ resources/views/cabinet/order_history.blade.php
@@ -20,7 +20,7 @@
                 @include('components.cabinet.menu', ['activeTab' => 'order_history'])
 
                 <div class="personal_data_change_box">
-                    <span class="text_800_32_black order_history_title">
+                    <span class="order_history_title">
                         {{__('bonuses_and_order_history.order_history.order_history')}}
                     </span>
 
