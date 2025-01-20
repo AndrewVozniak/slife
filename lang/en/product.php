4a98e6d73eb6a7a195e0acea2a2818007ed6a327
Zoriana <z.v.tomkiv@gmail.com>
2025-01-20 23:31:37 +0200

    popup cashback

--- lang/en/product.php
+++ lang/en/product.php
@@ -46,6 +46,15 @@ return [
         'product_quality' => 'The quality of our products is confirmed by a conclusion from the State Sanitary and Epidemiological Service of Ukraine.',
         'obtaining_certificate' => 'To obtain a certificate for any product, please send a request to our email, our managers will gladly provide all necessary information.',
         'popup_cashback_title' => 'Get cashback on purchases',
+        'popup_cashback_main_info' => 'For registered users only: for each purchase you receive cashback in the form of points that can be used for discounts on subsequent purchases',
+        'popup_cashback_how' =>'How it works',
+        'popup_cashback_text1' => 'For every 5000 UAH of order, 1% cashback in the form of points is accrued',
+        'popup_cashback_text2' => 'For example, when purchasing for UAH 10,000, cashback is 2%, for UAH 15,000, cashback is 3%, and so on',
+        'popup_cashback_text3' => 'Maximum cashback — 5%',
+        'popup_cashback_text4' => 'Points are deducted immediately after they are used',
+        'popup_cashback_info' =>'Register and receive bonuses for purchases',
+
+
     ],
 
     'reviews' => 'Reviews',
--- resources/views/components/product/product_description.blade.php
+++ resources/views/components/product/product_description.blade.php
@@ -112,11 +112,26 @@
                              class="img_24">
                     </div>
 
-{{--                    <div class="popup_cashback" x-show="popupCashback">--}}
-{{--                        <div class="popup_cashback_title">--}}
-{{--                            <span><b>{{__('product.details.popup_cashback_title')}}&excl;<b></b></span>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
+                    <div class="popup_cashback" x-show="popupCashback">
+                        <div class="popup_cashback_title">
+                            <p>{{__('product.details.popup_cashback_title')}}&excl;</p>
+                        </div>
+                        <div class="popup_cashback_text">
+                            <span>{{__('product.details.popup_cashback_main_info')}}&#46;</span>
+                        </div>
+                        <div class="popup_cashback_title" style="margin-top: 8px">
+                            <span>{{__('product.details.popup_cashback_how')}}&#58;</span>
+                        </div>
+                        <ul>
+                            <li>{{__('product.details.popup_cashback_text1')}}&#46;</li>
+                            <li>{{__('product.details.popup_cashback_text2')}}&#46;</li>
+                            <li>{{__('product.details.popup_cashback_text3')}}&#46;</li>
+                            <li>{{__('product.details.popup_cashback_text4')}}&#46;</li>
+                        </ul>
+                        <div class="popup_cashback_text">
+                            <span>{{__('product.details.popup_cashback_info')}}&excl;</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
